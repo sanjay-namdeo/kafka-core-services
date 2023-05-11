2cbd1dc8b47af1ee2d6e14bf2da1020906e843cb
Sanjay Namdeo <sanjay.namdeo@sas.com>
2023-05-11 16:08:54 +0530

    Added ProducerDemoKeys

---
+++ basics/src/main/java/com/kafka/basic/ProducerDemoKeys.java
@@ -0,0 +1,42 @@
+package com.kafka.basic;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemoKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Producer Demo With Callback Started");
+        // Create a producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(ProducerProperties.properties);
+
+        for (int i = 0; i < 10; i++) {
+            String key = "id" + i;
+            // Send a producer record
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", key, "Hello, world " + i);
+
+            // Send data - asynchronous operation
+            producer.send(producerRecord, (metadata, exception) -> {
+                if (exception == null) {
+                    log.info("Received new metadata: \nTopic - {} \nKey - {}\nPartition - {} \nOffset - {}, \nTimestamp - {}, ",
+                            metadata.topic(),
+                            producerRecord.key(),
+                            metadata.partition(),
+                            metadata.offset(),
+                            metadata.timestamp());
+                } else {
+                    log.error("Error while producing - {}", exception.getMessage());
+                }
+            });
+
+            Thread.sleep(100);
+        }
+
+        // Flush and close the producer
+        producer.close();
+        log.info("Producer Demo With Callback Finished");
+    }
+}
