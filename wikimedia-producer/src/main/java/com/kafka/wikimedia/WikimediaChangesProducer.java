c663be1c47c640554813182837f9a7c8a2416243
Sanjay Namdeo <sanjay.namdeo@sas.com>
2023-05-12 17:52:14 +0530

    Added new module

---
+++ wikimedia-producer/src/main/java/com/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,7 @@
+package com.kafka.wikimedia;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+        System.out.println("Hello, WikimediaChangesProducer!");
+    }
+}
